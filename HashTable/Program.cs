ffbb3f00227a72ee5636a228ecfbb7097305f6c7
CMD <gautamkailashsingh@gmail.com>
2020-10-27 01:40:35 +0530

    [Gautam] Implemented Hash Table data structure to find out the frequency of the occurrence of a word in a sentence.

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HashTable
 {
@@ -6,7 +7,32 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hash Table");
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+
+            string input = "to be or not to be";
+            Console.WriteLine("Input Statement is: ");
+            Console.WriteLine(input);
+            string[] inputArray = input.Split(" ");
+            foreach(string word in inputArray)
+            {
+                if (hash.Get(word) == 0)
+                {
+                    hash.Add(word, 1);
+                    
+                }
+                else
+                {
+                    int value = hash.Get(word) + 1;
+                    hash.Set(word, value);
+                }
+            }
+
+            string []newInputArray = inputArray.Distinct().ToArray();
+            foreach(var word in newInputArray)
+            {
+                Console.WriteLine("The frequency of occurrence of \"" + word + "\" is: " + hash.Get(word));
+            }
         }
     }
 }
